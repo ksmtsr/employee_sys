dae30d27c76a5f967cba82eded7dd560ab819036
s-kishimoto <sarii.4u@icloud.com>
2024-04-23 13:09:02 +0900

    モデル、deviseの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  
+  devise_for :admins
+  root to: "employees#index"
 
   # WebpackerがビルドしたJavaScriptファイルにアクセスするための適切なルートを設定
   get '/packs/js/application-468255184b399d3d4617.js', to: redirect('assets/application-468255184b399d3d4617.js')
